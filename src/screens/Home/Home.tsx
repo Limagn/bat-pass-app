47393d6c4eeea39d0aa811651cccb9ee7dfd7060
limagln <gabriellneiva@outlook.com>
2025-01-03 23:41:53 -0300

    Add Logo

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import styles from "./Style";
+import { Menu } from "../../components/Menu/Menu";
+import { BatLogo } from "../../components/BatLogo/BatLogo";
+
 
 export default function Home() {
   return(
     <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <BatLogo/>
+      </View>
+
       <Text>Hello Gabriel</Text>
       <StatusBar style="auto" />
     </View>
--- src/screens/Home/Style.tsx
+++ src/screens/Home/Style.tsx
@@ -8,6 +8,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  logoContainer: {
+  }
 });
 
 export default styles
